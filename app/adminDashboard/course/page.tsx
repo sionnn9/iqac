cbcdfac5c77f579ac6d793ed1a3858e88c65464e
Sion Aaron Lobo <154431317+sionnn9@users.noreply.github.com>
2025-07-16 18:16:55 +0530

    errr test

--- app/adminDashboard/course/page.tsx
+++ app/adminDashboard/course/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <div className="bg-gray-950 w-full h-16 flex items-center px-4 sm:px-6">
         <SidebarTrigger className="text-white mr-4" />
         <h1 className="text-white text-lg sm:text-xl font-semibold flex-1 text-center sm:text-left pr-9">
-          Courses
+          Coursess
         </h1>
       </div>
 
